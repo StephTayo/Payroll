26f3927da614deeca89d8fd89c2d1ee0b2874a9f
Stephen Omotayo <127545231+StephTayo@users.noreply.github.com>
2023-04-26 11:08:19 -0600

    Add files via upload

---
+++ Payroll/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Payroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPay = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRegular = new System.Windows.Forms.TextBox();
+            this.txtOvertime = new System.Windows.Forms.TextBox();
+            this.pnlOvertime = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlOvertime.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(123, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hours worked:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hourly rate:";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(289, 204);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(117, 35);
+            this.txtHours.TabIndex = 3;
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(289, 257);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(117, 35);
+            this.txtRate.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(89, 312);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(130, 41);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 478);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pay amount:";
+            // 
+            // txtPay
+            // 
+            this.txtPay.Location = new System.Drawing.Point(289, 478);
+            this.txtPay.Name = "txtPay";
+            this.txtPay.ReadOnly = true;
+            this.txtPay.Size = new System.Drawing.Size(117, 35);
+            this.txtPay.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Regular pay:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Overtime pay:";
+            // 
+            // txtRegular
+            // 
+            this.txtRegular.Location = new System.Drawing.Point(215, 13);
+            this.txtRegular.Name = "txtRegular";
+            this.txtRegular.ReadOnly = true;
+            this.txtRegular.Size = new System.Drawing.Size(117, 35);
+            this.txtRegular.TabIndex = 10;
+            // 
+            // txtOvertime
+            // 
+            this.txtOvertime.Location = new System.Drawing.Point(215, 60);
+            this.txtOvertime.Name = "txtOvertime";
+            this.txtOvertime.ReadOnly = true;
+            this.txtOvertime.Size = new System.Drawing.Size(117, 35);
+            this.txtOvertime.TabIndex = 11;
+            // 
+            // pnlOvertime
+            // 
+            this.pnlOvertime.Controls.Add(this.label4);
+            this.pnlOvertime.Controls.Add(this.txtOvertime);
+            this.pnlOvertime.Controls.Add(this.txtRegular);
+            this.pnlOvertime.Controls.Add(this.label5);
+            this.pnlOvertime.Location = new System.Drawing.Point(74, 359);
+            this.pnlOvertime.Name = "pnlOvertime";
+            this.pnlOvertime.Size = new System.Drawing.Size(366, 100);
+            this.pnlOvertime.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 543);
+            this.Controls.Add(this.pnlOvertime);
+            this.Controls.Add(this.txtPay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Payroll Calculation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlOvertime.ResumeLayout(false);
+            this.pnlOvertime.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRegular;
+        private System.Windows.Forms.TextBox txtOvertime;
+        private System.Windows.Forms.Panel pnlOvertime;
+    }
+}
+
---
+++ Payroll/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll
+{
+    /*
+     * Calculate pay amount based on hours worked and hourly rate.
+     * 37.5 hours constitute full week work.
+     * Employees who works more that full week gets paid “time and  a half” 
+     * for overtime hours
+     * Author: Jolanta
+     * When: March 2023
+     */
+    public partial class Form1 : Form
+    {
+        // class-level declarations
+        const decimal FULL_WEEK = 37.5m;
+        const decimal OT_RATE = 1.5m;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // at the start, hide panel with overtime controls
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnlOvertime.Visible = false;
+        }
+
+        // calculate pay amount based on hours worked and hourly pay
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // local declarations
+            decimal hours;      // input - hours worked
+            decimal rate;       // input - hourly rate
+            decimal regularPay; // straight time pay
+            decimal otPay;      // overtime pay
+            decimal payAmount;  // output - total pay amount
+
+            // get hours and rate
+            hours = Convert.ToDecimal(txtHours.Text);
+            rate = Convert.ToDecimal(txtRate.Text);
+
+            // calculate pay amount
+            if (hours <= FULL_WEEK) // no overtime
+            {
+                regularPay = hours * rate;
+                otPay = 0;        
+                pnlOvertime.Visible = false;
+            }
+            else // hours > FULL_WEEK - overtime applies
+            {
+                regularPay = FULL_WEEK * rate; 
+                otPay = (hours - FULL_WEEK) * rate * OT_RATE;    
+                pnlOvertime.Visible = true;
+            }
+            payAmount = regularPay + otPay;
+
+            // display results
+            txtRegular.Text = regularPay.ToString("c");
+            txtOvertime.Text = otPay.ToString("c");
+            txtPay.Text = payAmount.ToString("c");
+        }
+
+        
+    }
+}
